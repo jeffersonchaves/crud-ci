f3dc638d0f29e63fb68d92bcbcd27794ba8c46db
Jefferson Chaves <jeffersong.s@hotmail.com>
2021-01-19 16:23:02 -0300

    correção de arquivos para aula

--- src/main/java/br/edu/ifpr/entidades/Pessoa.java
+++ src/main/java/br/edu/ifpr/entidades/Pessoa.java
@@ -4,35 +4,5 @@ public class Pessoa {
 	
 	private String nome;
 	private Integer idade;
-
-	public void setNome(String nome) {
-		
-		if(nome == null || nome.isEmpty()) {
-			throw new IllegalArgumentException("nome nao pode ser nulo ou vazio");
-		}
-		
-		this.nome = nome;
-		
-	}
-
-	public void setIdade(Integer idade) {
-		
-		if(idade < 18) {
-			throw new IllegalArgumentException("nao pode ser menor de idade");
-		}
-		
-		this.idade = idade;
-		
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public Integer getIdade() {
-		return idade;
-	}
-	
-	
-
+	private String sexo;
 }
--- src/test/java/br/edu/ifpr/entidades/PessoaTest.java
+++ src/test/java/br/edu/ifpr/entidades/PessoaTest.java
@@ -1,33 +1,6 @@
 package br.edu.ifpr.entidades;
 
-import static org.junit.Assert.assertNotNull;
-
-import org.junit.Test;
-
 public class PessoaTest {
 	
-	
-	@Test
-	public void deve_criar_pessoa() {
-		Pessoa sut = new Pessoa();
-		
-		assertNotNull(sut);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void nome_nao_deve_ser_nulo_ou_vazio() {
-		Pessoa sut = new Pessoa();
-		
-		sut.setNome("");
-		
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void pessoa_nao_pode_ser_menor_de_idade() {
-		Pessoa sut = new Pessoa();
-		
-		sut.setIdade(17);
-		
-	}
 
 }
