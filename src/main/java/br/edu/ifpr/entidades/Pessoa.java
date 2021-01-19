a18d90700daf1f39b23a6c317e8bdb232def8cde
Jefferson Chaves <jeffersong.s@hotmail.com>
2021-01-19 17:02:29 -0300

    create Pessoa Entity

--- src/main/java/br/edu/ifpr/entidades/Pessoa.java
+++ src/main/java/br/edu/ifpr/entidades/Pessoa.java
@@ -5,4 +5,30 @@ public class Pessoa {
 	private String nome;
 	private Integer idade;
 	private String sexo;
+	
+	public Pessoa() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	
+	
 }
