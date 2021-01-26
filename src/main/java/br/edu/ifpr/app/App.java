64c75b8984513f94e4c0041e3f5c5c2095e2e01a
Jefferson Chaves <jeffersong.s@hotmail.com>
2021-01-26 17:14:58 -0300

    adequando projeto para aula

--- src/main/java/br/edu/ifpr/app/App.java
+++ src/main/java/br/edu/ifpr/app/App.java
@@ -1,14 +1,9 @@
 package br.edu.ifpr.app;
 
-import br.edu.ifpr.entidades.Pessoa;
-
 public class App {
 
 	public static void main(String[] args) throws Exception {
-		Pessoa p1 = new Pessoa();
 		
-		p1.setNome("jefferson");
-		p1.setIdade(32);
 	}
 	
 }
